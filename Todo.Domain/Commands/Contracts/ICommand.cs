3aef3693257b665b1f88a3c84f2578ba39dbc139
VictorSardellaro <victor.henryk@outlook.com>
2021-10-13 11:31:17 -0300

    Add flunt heritage

--- Todo.Domain/Commands/Contracts/ICommand.cs
+++ Todo.Domain/Commands/Contracts/ICommand.cs
@@ -1,7 +1,9 @@
+using Flunt.Validations;
+
 namespace Todo.Domain.Commands.Contracts
 {
-    public interface ICommand
+    public interface ICommand : IValidatable
     {
-        bool Validate();
+        
     }
 }
