a59d029330bf0405e12cface7c8e9df082abf526
VictorSardellaro <victor.henryk@outlook.com>
2021-10-14 11:48:56 -0300

    Updatetodocommand

---
+++ Todo.Domain/Commands/UpdateTodoCommand.cs
@@ -0,0 +1,32 @@
+using System;
+using Flunt.Notifications;
+using Flunt.Validations;
+using Todo.Domain.Commands.Contracts;
+
+namespace Todo.Domain.Commands
+{
+    public class UpadateTodoCommand : Notifiable, ICommand
+    {
+        public UpadateTodoCommand() { }
+        public UpadateTodoCommand(Guid id, string title, string user)
+        {
+            Id = id;
+            Title = title;
+            User = user;
+        }
+
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string User { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(
+                new Contract()
+                .Requires()
+                .HasMinLen(Title, 3, "Title", "Por favor, descreva melhor esta tarefa!")
+                .HasMinLen(User, 6, "User", "Usuário inválido!")
+            );
+        }
+    }
+}
