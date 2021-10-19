27f9e21c2667f8d1cd9c9413dea424f2299bc92c
VictorSardellaro <victor.henryk@outlook.com>
2021-10-19 18:53:49 -0300

    Handlers MarkTodoAs(Un)DoneComma

--- Todo.Domain/Handlers/TodoHandler.cs
+++ Todo.Domain/Handlers/TodoHandler.cs
@@ -10,7 +10,10 @@ namespace Todo.Domain.Handlers
     public class TodoHandler :
     Notifiable,
     IHandler<CreateTodoCommand>,
-    IHandler<UpdateTodoCommand>
+    IHandler<UpdateTodoCommand>,
+    IHandler<MarkTodoAsDoneCommand>,
+    IHandler<MarkTodoAsUndoneCommand>
+
     {
         private readonly ITodoRepository _repository;
         public TodoHandler(ITodoRepository repository)
@@ -60,6 +63,44 @@ namespace Todo.Domain.Handlers
 
         }
 
+        public ICommandResult Handle(MarkTodoAsDoneCommand command)
+        {
+            // Fail Fast Validation
+            command.Validate();
+            if (command.Invalid)
+                return new GenericCommandResult(false, "Ops, parece que sua tarefa está errada!", command.Notifications);
 
+            // Recupera o TodoItem
+            var todo = _repository.GetById(command.Id, command.User);
+
+            // Altera o estado
+            todo.MarkAsDone();
+
+            // Salva no banco
+            _repository.Update(todo);
+
+            // Retorna o resultado
+            return new GenericCommandResult(true, "Tarefa salva", todo);
+        }
+
+        public ICommandResult Handle(MarkTodoAsUndoneCommand command)
+        {
+            // Fail Fast Validation
+            command.Validate();
+            if (command.Invalid)
+                return new GenericCommandResult(false, "Ops, parece que sua tarefa está errada!", command.Notifications);
+
+            // Recupera o TodoItem
+            var todo = _repository.GetById(command.Id, command.User);
+
+            // Altera o estado
+            todo.MarkAsUndone();
+
+            // Salva no banco
+            _repository.Update(todo);
+
+            // Retorna o resultado
+            return new GenericCommandResult(true, "Tarefa salva", todo);
+        }
     }
 }
