9955765b99b59ef1893c69817a6985f87fbc7743
VictorSardellaro <victor.henryk@outlook.com>
2021-10-21 11:36:41 -0300

    AddTransient

--- Todo.Domain.Api/Startup.cs
+++ Todo.Domain.Api/Startup.cs
@@ -5,7 +5,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Todo.Domain.Handlers;
 using Todo.Domain.Infra.Contexts;
+using Todo.Domain.Respositories;
 
 namespace Todo.Domain.Api
 {
@@ -26,6 +28,8 @@ namespace Todo.Domain.Api
             services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
             // services.AddDbContext<DataContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("connectionString")));
 
+            services.AddTransient<ITodoRepository, TodoRepository>();
+            services.AddTransient<TodoHandler, TodoHandler>();
 
             services.AddSwaggerGen(c =>
             {
