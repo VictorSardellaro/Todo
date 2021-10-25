2494a9ce010c3cf074f49a2fedebc7a0dce3770b
VictorSardellaro <victor.henryk@outlook.com>
2021-10-25 11:35:32 -0300

    jwtbearer + firebase config

--- Todo.Domain.Api/Startup.cs
+++ Todo.Domain.Api/Startup.cs
@@ -1,9 +1,11 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Todo.Domain.Handlers;
 using Todo.Domain.Infra.Contexts;
@@ -36,7 +38,31 @@ namespace Todo.Domain.Api
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Todo.Domain.Api", Version = "v1" });
             });
+
+            services
+               .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+               .AddJwtBearer(options =>
+               {
+                   options.Authority = "https://securetoken.google.com/todos-f2227";
+                   options.TokenValidationParameters = new TokenValidationParameters
+                   {
+                       ValidateIssuer = true,
+                       ValidIssuer = "https://securetoken.google.com/todos-f2227",
+                       ValidateAudience = true,
+                       ValidAudience = "todos-f2227",
+                       ValidateLifetime = true
+                   };
+               });
         }
+
+        //         const firebaseConfig = {
+        //     apiKey: "AIzaSyDb0uKHjWCMMIaQ1UXtlnCvOjRbAQdIq5M",
+        //     authDomain: "todos-f2227.firebaseapp.com",
+        //     projectId: "todos-f2227",
+        //     storageBucket: "todos-f2227.appspot.com",
+        //     messagingSenderId: "1029398351541",
+        //     appId: "1:1029398351541:web:06117a51c92c797273b8fc"
+        //   };
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
